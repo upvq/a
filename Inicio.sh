50d99c534e126a0c19d610a75336d1767d19a5c5
upvq <94366096+upvq@users.noreply.github.com>
2021-11-15 13:13:55 +0100

    Update Inicio.sh

--- Inicio.sh
+++ Inicio.sh
@@ -14,3 +14,59 @@ sudo apt install php7.4 libapache2-mod-php7.4 php7.4-mysql php-common php7.4-cli
 sudo a2enmod php7.4
 
 sudo systemctl restart apache2
+
+cd /var/www
+
+mkdir dominio1 & cd dominio1 & mkdir index.html & cd ../
+
+mkdir dominio2 & cd dominio2 & mkdir index.html & cd ../
+
+cd /etc/apache2/sites-available/ 
+
+exec 3<> dominio1
+
+    # Let's print some text to fd 3
+    echo "#web de dominio1" >&3
+    echo "<VirtualHost *:80" >&3
+    echo "ServerName 192.168.115.11" >&3
+    echo "ServerAlias dominio1.com" >&3
+    echo "DocumentRoot /var/www/dominio1" >&3
+    echo "DirectoryIndex index.html" >&3
+    echo "ServerAdmin contacto@dominio1.com
+    echo "</VirtualHost>" >&3
+
+# Close fd 3
+exec 3>&-
+
+exec 3<> dominio2
+
+    # Let's print some text to fd 3
+    echo "#web de dominio2" >&3
+    echo "<VirtualHost *:80" >&3
+    echo "ServerName 192.168.115.11" >&3
+    echo "ServerAlias dominio2.com" >&3
+    echo "DocumentRoot /var/www/dominio2" >&3
+    echo "DirectoryIndex index.html" >&3
+    echo "ServerAdmin contacto@dominio1.com
+    echo "</VirtualHost>" >&3
+
+# Close fd 3
+exec 3>&-
+
+a2ensite dominio1
+
+a2ensite dominio2
+
+/etc/init.d/apache2 restart
+
+cd /
+cd /etc
+
+exec 3<> hosts
+
+    # Let's print some text to fd 3
+    echo "dominio1.com 192.168.15.11" >&3
+    echo "dominio2.com 192.168.15.11" >&3
+
+# Close fd 3
+exec 3>&-
