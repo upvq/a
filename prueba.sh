e17f1778dd133eef04fd30dc29eb3a40c1ba6689
upvq <94366096+upvq@users.noreply.github.com>
2021-11-18 08:43:53 +0100

    Update prueba.sh

--- prueba.sh
+++ prueba.sh
@@ -3,9 +3,10 @@ sudo apt upgrade
 sudo apt install apache2 -y
 sudo apt install mysql-server -y
 sudo /usr/bin/mysql_secure_installation
-sudo apt install php libapache2-mod-php php-mysql
+sudo apt install php libapache2-mod-php php-mysql -y
 sudo nano /etc/apache2/mods-enabled/dir.conf
 sudo apt-cache search php-
-sudo apt-get install <<ModuleName>>
 
 sudo service apache2 restart
+
+mysql -u root -p
