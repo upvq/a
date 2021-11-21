754acad3e8e90bca3e552dcd64817892fc06a337
upvq <94366096+upvq@users.noreply.github.com>
2021-11-21 19:19:03 +0100

    Update prueba.sh

--- prueba.sh
+++ prueba.sh
@@ -1,12 +1,23 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install apache2 -y
-sudo apt install mysql-server -y
-sudo /usr/bin/mysql_secure_installation
-sudo apt install php libapache2-mod-php php-mysql -y
-sudo nano /etc/apache2/mods-enabled/dir.conf
-sudo apt-cache search php-
-
 sudo service apache2 restart
-
+apt-get -y update
+apt-get -y install graphviz aspell ghostscript clamav git
+apt-get -yupdate
+apt-get -y install mysql-server mysql-client
+apt-get -y update
+apt install -y net-tools
+apt-get -y install apache2 php libapache2-mod-php php-cli php-mysql php-mbstring php-xmlrpc php-zip
+apt-get -y install php-gd php-xml php-bcmath php-ldap php-pspell  php-curl php-intl php-soap
+service apache2 restart
+mkdir downloads
+cd downloads
+wget https://wordpress.org/latest.zip
+apt-get install unzip
+unzip -q latest.zip -d /var/www/html/
+cd /var/www/html/wordpress
+chown www-data. -R /var/www/html/wordpress
+apt install -y gnome 
 mysql -u root -p
+
